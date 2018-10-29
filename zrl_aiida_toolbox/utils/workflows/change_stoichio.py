6242df71f626de2a2f1c85f872b2f9631bbdca42
Tobias <tbi@zurich.ibm.com>
2018-10-29 17:33:52 +0100

    Commit 29 Oct 2018

--- zrl_aiida_toolbox/utils/workflows/change_stoichio.py
+++ zrl_aiida_toolbox/utils/workflows/change_stoichio.py
@@ -72,7 +72,15 @@ class ChangeStoichiometryWorkChain(WorkChain):
         structure = self.inputs.structure
         species = self.inputs.species.value
         delta_N = self.inputs.deltaN.value
+        delta_weight_equal = 0.01
         error_tol_occ = 1e-4
+
+        if (abs(delta_N) <= error_tol_occ):
+            print('ERROR')
+
+        positive_change = True
+        if (delta_N < 0.0):
+            positive_change = False
 
         fixed_sites = []
         species_weight_sites = {}
@@ -80,53 +88,105 @@ class ChangeStoichiometryWorkChain(WorkChain):
             site = structure.sites[i]
             kind = structure.get_kind(site.kind_name)
 
-            found_species = False
             weight_species = 0.0
             weight_total = 0.0
+            species_count = 0
             for j in range(len(kind.symbols)):
                 weight_total += kind.weights[j]
                 if (kind.symbols[j] == species):
-                    found_species = True
+                    species_count += 1
+                    species_index = j
                     weight_species += kind.weights[j]
 
-            if found_species and (weight_total < 1.0):
+            if (species_count > 1):
+                    print('ERROR')
+
+            if (species_count == 1):
                 if (weight_species not in species_weight_sites.keys()):
                     species_weight_sites[weight_species] = []
-                species_weight_sites[weight_species].append(i)
+                species_weight_sites[weight_species].append([i, species_index, weight_species, weight_total])
             else:
                 fixed_sites.append(i)
 
         species_weights = species_weight_sites.keys()
-
-        species_weights.sort(reverse=True)
+        species_weights.sort(reverse=positive_change)
+        recheck = True
+        while recheck:
+            recheck = False
+            for i in range(1, len(species_weights)): 
+                if (abs(species_weights[i] - species_weights[i-1]) <= delta_weight_equal):
+                    if (len(species_weight_sites[species_weights[i]]) > 0):
+                        recheck = True
+                    species_weight_sites[species_weights[i-1]] += species_weight_sites[species_weights[i]]
+                    species_weight_sites[species_weights[i]] = []
 
         struct_out = StructureData(cell=structure.cell)
 
         atms_to_add = delta_N
         for weight in species_weights:
+            num_sites = len(species_weight_sites[weight])
+            weight_total_dict = {}
             for i in species_weight_sites[weight]:
-                site = structure.sites[i]
-                kind = structure.get_kind(site.kind_name)
-                weight_species = 0.0
-                weight_total = 0.0
-                species_count = 0
-                for j in range(len(kind.symbols)):
-                    weight_total += kind.weights[j]
-                    if (kind.symbols[j] == species):
-                        species_count += 1
-                        species_index = j
-                        weight_species += kind.weights[j]
+                site_index = i[0]
+                species_index = i[1]
+                weight_species = i[2]
+                weight_total = i[3]
+                if (weight_total not in weight_total_dict.keys()):
+                    weight_total_dict[weight_total] = []
+                weight_total_dict[weight_total].append([site_index, species_index, weight_species, weight_total])
 
-                if (abs(weight_species - weight) > error_tol_occ) or (weight_total - 1.0 > error_tol_occ) \
-                    or (species_count != 1):
-                    print('ERROR')
+                if not positive_change:
+                    if (num_sites > 0):
+                        atms_to_add_per_site = atms_to_add / num_sites
+                    else:
+                        atms_to_add_per_site = 0
 
-                change = min([atms_to_add, (1.0-weight_total)])
-                atms_to_add = atms_to_add - change
+                    site = structure.sites[site_index]
+                    kind = structure.get_kind(site.kind_name)
 
-                weights_out = list(kind.weights)
-                weights_out[species_index] += change
-                struct_out.append_atom(position=site.position, symbols=kind.symbols, weights=weights_out)
+                    if (atms_to_add_per_site > 0.0 + error_tol_occ) or (weight_species < 0.0 - error_tol_occ):
+                        print('ERROR')
+
+                    change = max([atms_to_add_per_site, -weight_species])
+                    atms_to_add = atms_to_add - change
+                    num_sites -= 1
+
+                    weights_out = list(kind.weights)
+                    weights_out[species_index] += change
+                    weight_total_out = weight_total + change
+                    if (weights_out[species_index] < 0.0):
+                        weights_out[species_index] = 0.0
+                    if (weight_total_out > 0.0 + error_tol_occ):
+                        struct_out.append_atom(position=site.position, symbols=kind.symbols, weights=weights_out)
+
+            if positive_change:
+                weight_total_list = weight_total_dict.keys()
+                weight_total_list.sort(reverse=True)
+
+                for weight_total in weight_total_list:
+                    for i in weight_total_dict[weight_total]:
+                        if (num_sites > 0):
+                            atms_to_add_per_site = atms_to_add / num_sites
+                        else:
+                            atms_to_add_per_site = 0
+
+                        site_index = i[0]
+                        species_index = i[1]
+                        weight_species = i[2]
+                        weight_total = i[3]
+                        site = structure.sites[site_index]
+                        kind = structure.get_kind(site.kind_name)
+
+                        if (weight_total > 1.0 + error_tol_occ):
+                            print('ERROR')
+
+                        change = min([atms_to_add_per_site, (1.0-weight_total)])
+                        atms_to_add = atms_to_add - change
+                        num_sites -= 1
+
+                        weights_out = list(kind.weights)
+                        weights_out[species_index] += change
+                        struct_out.append_atom(position=site.position, symbols=kind.symbols, weights=weights_out)
 
         if (abs(atms_to_add) > error_tol_occ):
             print('ERROR')
@@ -136,6 +196,8 @@ class ChangeStoichiometryWorkChain(WorkChain):
                 site = structure.sites[i]
                 kind = structure.get_kind(site.kind_name)
                 struct_out.append_atom(position=site.position, symbols=kind.symbols, weights=kind.weights)
+
+        struct_out.__dict__
 
     
     def set_output(self):
