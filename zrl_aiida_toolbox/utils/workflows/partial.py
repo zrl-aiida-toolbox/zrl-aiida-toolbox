d5d7a6d01aa5439502d7a573076be7e8f50fc519
Matthieu Mottet <m.mottet@outlook.com>
2018-11-28 11:29:13 +0100

    Refactored initialization of the partial_occ workchain

--- zrl_aiida_toolbox/utils/workflows/partial.py
+++ zrl_aiida_toolbox/utils/workflows/partial.py
@@ -54,14 +54,18 @@ class PartialOccupancyWorkChain(WorkChain):
         }
         
         self.ctx.structure = self.inputs.structure.get_pymatgen()
-        if self.inputs.verbose:
-            self.report(self.ctx.structure.formula)
+        
+        for element in self.ctx.structure.composition:
+            composition = self.ctx.structure.composition.get(element)
+            if composition != np.floor(composition):
+                self.report('Warning: the input structure has a non integer composition for element `%s`, you '
+                            'might be loosing an atom.' % element)
             
-        self.ctx.partials = []
-        for site in self.ctx.structure:
-            species = site.species_and_occu.as_dict().items()
-            if (len(species) > 1 or species[0][1] < 1) and species not in self.ctx.partials:
-                self.ctx.partials.append(species)
+#         self.ctx.partials = []
+#         for site in self.ctx.structure:
+#             species = site.species_and_occu.as_dict().items()
+#             if (len(species) > 1 or species[0][1] < 1) and species not in self.ctx.partials:
+#                 self.ctx.partials.append(species)
         
         self.ctx.vacancy = Specie(parameter_dict.get('vacancy_ion') if 'vacancy_ion' in parameter_dict else 'Lr', 0)
         
@@ -86,22 +90,88 @@ class PartialOccupancyWorkChain(WorkChain):
         with either the ion, or the vacancy site.
         :return:
         """            
-        self.ctx.static = [
-            PeriodicSite(Specie(site.species_and_occu.items()[0][0].value),
-                         site.coords, site.lattice, True, True)
-            for site in self.ctx.structure if site.species_and_occu.as_dict().items() not in self.ctx.partials
-        ]
         
+        
+        fixed_counts = {}
+        partial_counts = {}
+        
+        self.ctx.static = []
+        self.ctx.partial = {}
+        
+        # We collect the sites into their composition and calculate the theoretical number of occupied sites
+        for site in self.ctx.structure:
+            if self.__is_partial(site):
+                self.ctx.partial.setdefault(site.species_and_occu, [[]])
+                self.ctx.partial.get(site.species_and_occu)[0].append(site)
+                
+                partial_counts.setdefault(site.species_and_occu, [[0, 0] for s in site.species_and_occu])
+                
+                for i, element in enumerate(site.species_and_occu):
+                    partial_counts[site.species_and_occu][i][0] += site.species_and_occu.get(element)
+                    partial_counts[site.species_and_occu][i][1] += site.species_and_occu.get(element)
+            else:
+                self.ctx.static.append(PeriodicSite(site.specie, site.coords, site.lattice, True, True))
+                fixed_counts.setdefault(site.specie, 0)
+                fixed_counts[site.specie] += 1
+        
+        # If all sites are static, then no need to do anything.
         if len(self.ctx.static) == len(self.ctx.structure):
             self.ctx.do_break = 0
             self.out('structures.%s' % self.inputs.structure.uuid, self.inputs.structure)
             return
+        
+        # We compile the number of occupied site for each partial composition while not going over the theoretical number
+        for comp in partial_counts:
+            self.ctx.rs.shuffle(self.ctx.partial.get(comp)[0])
+            for i, sp in enumerate(comp):
+                partial_counts[comp][i][0] = np.floor(partial_counts[comp][i][0])
+        
+        # Calculation of the departure from the composition. 
+        error = {
+            el: self.ctx.structure.composition.get(el) - fixed_counts.get(el, 0)
+            for el in self.ctx.structure.composition
+        }
 
-        q, sites = reduce(lambda q, acc: self.__sites_reducer(q, acc), self.ctx.structure, (0, {}))
+        for comp in partial_counts:
+            for i, sp in enumerate(comp):
+                error[sp] -= partial_counts.get(comp)[i][0]
 
+        # Adding ions to sites with the highest departure from theoretical number as long as the error
+        # is greater than 0.5.
+        for element in error:
+            while error[element] > 0.5:
+                if error[element] > 0:
+                    max_error = (None, 0)
+                    for i, comp in enumerate(partial_counts):
+                        if element in comp:
+                            for j, sp in enumerate(comp):
+                                if sp == element:
+                                    err = (partial_counts.get(comp)[j][1] - partial_counts.get(comp)[j][0]) ** 2
+                                    if err > max_error[1]:
+                                        max_error = ((comp, j), err)
+                    partial_counts.get(max_error[0][0])[max_error[0][1]][0] += 1
+                    error[element] -= 1
+        
         self.ctx.configurations = tuple()
-        self.ctx.sites = dict()
-
+        # self.ctx.sites = dict()
+        
+        for comp in partial_counts:
+            for i, sp in enumerate(comp):
+                for _ in range(int(partial_counts.get(comp)[i][0])):
+                    site = self.ctx.partial.get(comp)[-1].pop(0)
+                    self.ctx.partial.get(comp).insert(0, PeriodicSite(Specie(sp, self.ctx.charges.get(sp.value, 0)), 
+                                                                      site.coords, site.lattice, True, True))
+            leftovers = self.ctx.partial.get(comp).pop()
+            for site in leftovers:
+                self.ctx.partial.get(comp).insert(0, PeriodicSite(self.ctx.vacancy, 
+                                                                  site.coords, site.lattice, True, True))
+                # self.ctx.partial.get(comp)
+        self.ctx.sites = self.ctx.partial
+        del self.ctx.partial
+        
+        self.ctx.energy = [self.__ewald(self.ctx.sites)]
+        
+        return 
         counts = {}
         for total, species in sorted(sites.keys(), key=lambda x: x[0], reverse=True):
             print(total, species)
@@ -188,13 +258,20 @@ class PartialOccupancyWorkChain(WorkChain):
     def __ewald(self, sites):
         structure = Structure\
             .from_sites(sum([
-                filter(lambda v: v.specie.element.value != self.ctx.vacancy.element.value, value)
+                list(filter(lambda v: v.specie.element.value != self.ctx.vacancy.element.value, value))
                 for value in sites.values()
             ], []))
 
         return EwaldSummation(structure).total_energy
+    
+    def __is_partial(self, site):
+        for element in site.species_and_occu:
+            if site.species_and_occu.get(element) == 1:
+                return False    
+        return True
 
-    def __sites_reducer(self, (q, acc), cur):
+    def __sites_reducer(self, acc, cur):
+        q, acc = acc
         species = tuple(cur.species_and_occu.items())
         if len(species) > 1 or species[0][1] < 1:
             acc.setdefault((sum([x[1] for x in species]), species), [])
