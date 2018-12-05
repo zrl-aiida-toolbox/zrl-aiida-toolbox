ca504a6728fb08f1fd8e72be49e68c404f9e2709
Tobias <tbi@zurich.ibm.com>
2018-12-05 18:04:14 +0100

    Changed ReplicateWorkChain.

--- zrl_aiida_toolbox/utils/workflows/replicate.py
+++ zrl_aiida_toolbox/utils/workflows/replicate.py
@@ -19,7 +19,7 @@ class ReplicateWorkChain(WorkChain):
         spec.outline(
             cls.validate_inputs,
             if_(cls.abc_not_defined)(
-                cls.calculate_factors
+                cls.determine_factors
             ),
             cls.replicate
         )
@@ -38,32 +38,95 @@ class ReplicateWorkChain(WorkChain):
         self.ctx.max_electrons = int(parameter_dict.get('max_electrons')) if 'max_electrons' in parameter_dict else None
         self.ctx.max_volume = float(parameter_dict.get('max_volume')) if 'max_volume' in parameter_dict else None
         
+        self.ctx.min_electrons = int(parameter_dict.get('min_electrons')) if 'min_electrons' in parameter_dict else None
+        self.ctx.min_volume = float(parameter_dict.get('min_volume')) if 'min_volume' in parameter_dict else None
+        
         self.ctx.structure = self.inputs.structure.get_pymatgen()
 
         assert self.ctx.a and self.ctx.b and self.ctx.c \
                or (self.ctx.max_electrons and len(self.ctx.val_electrons)) \
-               or self.ctx.max_volume, \
-            'You must either provide values for `a`, `b` and `c` or for `max_electrons` in the parameters dictionary.'
+               or self.ctx.max_volume \
+               or (self.ctx.min_electrons and len(self.ctx.val_electrons)) \
+               or self.ctx.min_volume, \
+            'You must either provide values for `a`, `b` and `c` ' \
+            'or for `max_electrons` or `min_electrons`' \
+            'or for `max_volume` or `min_volume` in the parameters dictionary.'
 
     def abc_not_defined(self):
         return self.ctx.a is None and self.ctx.b is None and self.ctx.c is None
 
-    def calculate_factors(self):
+    def determine_factors(self):
         volume = self.ctx.structure.volume
 
+        max_volume = None
+        min_volume = None
         if self.ctx.max_electrons:
-            electrons = np.floor(sum(n * self.ctx.val_electrons.get(symbol)
-                                     for symbol, n in self.ctx.structure.composition.as_dict().items()))
+            electrons = sum(n * self.ctx.val_electrons.get(symbol)
+                                     for symbol, n in self.ctx.structure.composition.as_dict().items())
             max_volume = volume * self.ctx.max_electrons / electrons
-        else:
+        if self.ctx.min_electrons:
+            electrons = sum(n * self.ctx.val_electrons.get(symbol)
+                                     for symbol, n in self.ctx.structure.composition.as_dict().items())
+            min_volume = volume * self.ctx.min_electrons / electrons
+        if self.ctx.max_volume:
             max_volume = self.ctx.max_volume
+        if self.ctx.min_volume:
+            min_volume = self.ctx.min_volume
 
-        max_edge = np.power(max_volume, 1. / 3)
-        replicas = np.floor(max_edge / np.diag(self.ctx.structure.lattice.matrix)).astype(int)
+        if max_volume:
+            replicas = self.__calculate_factors(target_volume=max_volume, 
+                                                                          cell_matrix=self.ctx.structure.lattice.matrix, 
+                                                                          as_max=True)
+        if min_volume:
+            replicas = self.__calculate_factors(target_volume=min_volume, 
+                                                                          cell_matrix=self.ctx.structure.lattice.matrix, 
+                                                                          as_max=False)
+        
         replicas[np.where(replicas == 0)] = 1
-
         self.ctx.a, self.ctx.b, self.ctx.c = replicas
 
     def replicate(self):
         self.out('structure',
                  StructureData(pymatgen=self.ctx.structure * np.array([self.ctx.a, self.ctx.b, self.ctx.c], dtype=int)))
+                
+    def __calculate_factors(self, target_volume, cell_matrix, as_max):
+        norm_cell_matrix = []
+        cell_lengths = []
+        for i in [0,1,2]:
+            norm_cell_matrix.append(np.array(cell_matrix[i])/np.linalg.norm(cell_matrix[i]))
+            cell_lengths.append(np.linalg.norm(cell_matrix[i]))
+        volume_norm_cell = np.linalg.det(norm_cell_matrix)
+        target_edge = np.power(target_volume / volume_norm_cell, 1.0/3)
+        target_factors = target_edge / np.array(cell_lengths)
+        if as_max:
+            factors = np.ceil(target_factors).astype(int)
+            sign = -1
+        else:
+            factors = np.floor(target_factors).astype(int)
+            sign = 1
+        
+        new_cell_matrix = []
+        cell_lengths_new = []
+        for i in [0,1,2]:
+            new_cell_matrix.append(np.array(cell_matrix[i])*factors[i])
+            cell_lengths_new.append(np.linalg.norm(new_cell_matrix[i]))
+        volume_new_cell = np.linalg.det(new_cell_matrix)
+        n_round = 0
+        while (sign*volume_new_cell) < (sign*target_volume):
+            n_round += 1
+            change_side = 0
+            edge_difference = np.abs(cell_lengths_new[0] + sign*cell_lengths[0] - target_edge)
+            for i in [1,2]:
+                edge_difference_test = np.abs(cell_lengths_new[i] + sign*cell_lengths[i] - target_edge)
+                if edge_difference_test < edge_difference:
+                    change_side = i
+                    edge_difference = edge_difference_test
+            factors[change_side] += sign
+            for i in [0,1,2]:
+                new_cell_matrix[i] = np.array(cell_matrix[i])*factors[i]
+                cell_lengths_new[i] = np.linalg.norm(new_cell_matrix[i])
+            volume_new_cell = np.linalg.det(new_cell_matrix)
+            if n_round >= 10:
+                break
+ 
+        return list(factors)
